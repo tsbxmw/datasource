5a111f99b78110e5d6989e2a89597458ebf0e3ff
tsbxmw <1050636648@qq.com>
2019-12-25 16:51:23 +0800

    update deploy file

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,8 +5,8 @@ go build -o dist/auth ../apps/auth/auth.go
 go build -o dist/data ../apps/data/data.go
 
 
-docker build -f Dockerfile-auth mengwei2275/datasource-auth:v1 .
-docker build -f Dockerfile-data mengwei2275/datasource-data:v1 .
+docker build -f Dockerfile-auth -t mengwei2275/datasource-auth:v1 .
+docker build -f Dockerfile-data -t mengwei2275/datasource-data:v1 .
 
 docker push mengwei2275/datasource-auth:v1
 docker push mengwei2275/datasource-data:v1
