72eb25ffbea60c853377513608b90b016e8663b1
tsbxmw <1050636648@qq.com>
2019-12-25 16:33:01 +0800

    add deploy info

---
+++ deploy/build.sh
@@ -0,0 +1,12 @@
+mkdir dist
+
+go build -o dist/auth ../apps/auth/auth.go
+
+go build -o dist/data ../apps/data/data.go
+
+
+docker build -f Dockerfile-auth mengwei2275/datasource-auth:v1 .
+docker build -f Dockerfile-data mengwei2275/datasource-data:v1 .
+
+docker push mengwei2275/datasource-auth:v1
+docker push mengwei2275/datasource-data:v1
