d9381573ee3bfb8f91563d66cd25ea812c2449f1
tsbxmw <1050636648@qq.com>
2020-01-03 12:01:09 +0800

    add docker build push judgement

--- deploy/build.sh
+++ deploy/build.sh
@@ -8,5 +8,7 @@ go build -o dist/data ../apps/data/data.go
 docker build -f Dockerfile-auth -t mengwei2275/datasource-auth:$1 .
 docker build -f Dockerfile-data -t mengwei2275/datasource-data:$1 .
 
-docker push mengwei2275/datasource-auth:$1
-docker push mengwei2275/datasource-data:$1
+if [ $2 == "push" ]; then
+    docker push mengwei2275/datasource-auth:$1
+    docker push mengwei2275/datasource-data:$1
+fi
